d8060345dd8841c5431c8b41ca9f79686ac5dd6e
mike <mikemps@gmail.com>
2016-07-24 17:02:52 -0400

    making the scope an array ... sweet!!

--- rb/spec/game_of_life.rb
+++ rb/spec/game_of_life.rb
@@ -3,27 +3,23 @@ require_relative '../lib/corals'
 describe 'Game of Life' do
 
   subject { Corals.new [
-    { cells: [] },
+    [],
     {
-      cells: {
-        when: { x:1, y:1},
-        center?: true
-    }}
+      when: { x:1, y:1},
+      center?: true
+    }
   ]}
 
   it 'Dead cell with 0 neighbors stays dead' do
-    subject(
-      given: {
-        cells:[
-          [0,0,0],
-          [0,0,0],
-          [0,0,0]
-        ]},
-      cells: {
-        when: -> { center? },
-        then: -> { expect(it).to be_dead }
-      }
-    )
+    subject << {
+      given: [
+        [0,0,0],
+        [0,0,0],
+        [0,0,0]
+      ],
+      when: -> { center? },
+      then: -> { expect(self).to be_dead }
+    }
   end
 
 end
