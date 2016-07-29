71acf004d9453c80ac5a4723408bf52da9f6f551
mike <mikemps@gmail.com>
2016-07-29 11:03:57 -0400

    proper spec now

--- rb/spec/game_of_life.rb
+++ rb/spec/game_of_life.rb
@@ -7,28 +7,34 @@ describe 'Game of Life' do
     { when: 0, dead?: true },
     { when: 1, alive?: true },
     {
+      given: { size: -> { Math.sqrt count }},
+      x: -> { self / size },
+      y: -> { self % size }
+    },
+    {
       given: {
         delta: -> (x, y) { (x - y) ** 2 },
         distance: -> (x, y) { delta(x.x, y.x) + delta(x.y, y.y) },
         is_neighbor?: -> (x) { distance(self, x) == 0 }
       },
-      neighbors: -> { count is_neighbor? }
+      neighbors: -> { count is_neighbor? },
+      center?: -> { distance(self, first) == distance(self, last) }
     },
     { when: -> { dead? and neighbors < 3 }, dead?: true },
     { when: -> { dead? and neighbors > 3 }, alive?: true }
   ]}
 
-  def center; subject[4] end
-
   it 'Dead cell with 0 neighbors stays dead' do
 
-    subject << [
-      0, 0, 0,
-      0, 0, 0,
-      0, 0, 0
-    ]
-
-    expect(center).to be_dead
+    subject << {
+      given: [
+        0, 0, 0,
+        0, 0, 0,
+        0, 0, 0
+      ],
+      when: -> { center? },
+      then: -> { expect.to be_dead }
+    }
 
   end
 
