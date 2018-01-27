68aeb699763c781a729bfd099eed1a75e0230ac4
mike <mikemps@gmail.com>
2018-01-27 16:54:07 -0500

    much cleaner code

--- rb/spec/corals/mastermind.rb
+++ rb/spec/corals/mastermind.rb
@@ -1,23 +1,5 @@
 define mastermind: {
   rules: [
-    # {
-    #   blacks: {
-    #     zip: [:guess, :solution],
-    #     select: [:==],
-    #     count: []
-    #   }
-    # },
-    {
-      # given: {
-      #   missed: { zip: [:guess, :solution], select: :!= },
-      #   unknown: { map: [:missed, -> (guess, _) { guess }]},
-      #   unsolved: { map: [:missed, -> (_, solution) { solution }]},
-      #   misplaced: { :& => [:unknown, :unsolved] },
-      #   per_color: -> (color) {{ min: [{ count: [:unknown, color]}, { count: [:unsolved, color]}]}},
-      #   total: { map: [:misplaced, :per_color], reduce: [0, :+] },
-      #   whites: { :* => [[:white], :total] }
-      # }
-    },
     {
       given: {
         same: -> { -> (x,y) { x == y }},
