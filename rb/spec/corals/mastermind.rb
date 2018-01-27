092cb90a32271bbc5081dc49e3de27ccb528e5dd
mike <mikemps@gmail.com>
2018-01-27 16:52:55 -0500

    much cleaner code

--- rb/spec/corals/mastermind.rb
+++ rb/spec/corals/mastermind.rb
@@ -1,44 +1,38 @@
 define mastermind: {
   rules: [
+    # {
+    #   blacks: {
+    #     zip: [:guess, :solution],
+    #     select: [:==],
+    #     count: []
+    #   }
+    # },
     {
-      given: {
-        guessed: -> { guess.zip(solution).select { |guess, solution| guess == solution }},
-        blacks: -> { guessed.map { :black }},
-      }
+      # given: {
+      #   missed: { zip: [:guess, :solution], select: :!= },
+      #   unknown: { map: [:missed, -> (guess, _) { guess }]},
+      #   unsolved: { map: [:missed, -> (_, solution) { solution }]},
+      #   misplaced: { :& => [:unknown, :unsolved] },
+      #   per_color: -> (color) {{ min: [{ count: [:unknown, color]}, { count: [:unsolved, color]}]}},
+      #   total: { map: [:misplaced, :per_color], reduce: [0, :+] },
+      #   whites: { :* => [[:white], :total] }
+      # }
     },
     {
       given: {
-        blacks: {
-          zip: [:guess, :solution],
-          select: :==,
-          map: :black
-        }
+        same: -> { -> (x,y) { x == y }},
+        diff: -> { -> (x,y) { x != y }},
+        attempt: -> { guess.zip solution },
+        guessed: -> { attempt.select &same },
+        missed: -> { attempt.select(&diff).transpose },
+        misplaced: -> { missed.reduce :& },
+        count_per_color: -> { -> (color) { missed.map {|x| x.count color }.min } },
       }
     },
     {
-      given: {
-        missed: { zip: [:guess, :solution], select: :!= },
-        unknown: { map: [:missed, -> (guess, _) { guess }]},
-        unsolved: { map: [:missed, -> (_, solution) { solution }]},
-        misplaced: { :& => [:unknown, :unsolved] },
-        per_color: -> (color) {{ min: [{ count: [:unknown, color]}, { count: [:unsolved, color]}]}},
-        total: { map: [:misplaced, :per_color], reduce: [0, :+] },
-        whites: { :* => [[:white], :total] }
-      }
-    },
-    {
-      given: {
-        missed: -> { guess.zip(solution).select { |guess, solution| guess != solution }},
-        unknown: -> { missed.map { |guess, _| guess } },
-        unsolved: -> { missed.map { |_, solution| solution }},
-        misplaced: -> { unknown & unsolved },
-        per_color: -> (color) { [unknown.count(color), unsolved.count(color)].min },
-        count: -> { misplaced.map(per_color).reduce 0, :+},
-        whites: -> { [:white] * count }
-      }
-    },
-    {
-      feedback: -> { blacks + whites }
+      blacks: -> { guessed.count },
+      whites: -> { misplaced.map(&count_per_color).reduce 0, :+},
+      feedback: -> { [:black] * blacks + [:white] * whites }
     }
   ]
 }
