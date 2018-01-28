69e9bb81acd76c41621affd328feba4135977cbb
mike <mikemps@gmail.com>
2018-01-28 12:50:49 -0500

    π

--- rb/spec/corals/mastermind.rb
+++ rb/spec/corals/mastermind.rb
@@ -7,9 +7,7 @@ define mastermind: {
         missed: -> { attempt.select{|x,y|x!=y}.transpose },
         count_per_color: -> { -> color { missed.map {|x| x.count color }.min } },
         whites: -> { missed.reduce(:&).map(&count_per_color).reduce 0, :+},
-      }
-    },
-    {
+      },
       feedback: -> { [:black] * blacks + [:white] * whites }
     }
   ]
