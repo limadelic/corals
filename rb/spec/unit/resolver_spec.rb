ea67e070e730e25ea120f57e39a6edffdb977131
mike <mikemps@gmail.com>
2018-02-26 20:50:27 -0500

    moar specs

--- rb/spec/unit/resolver_spec.rb
+++ rb/spec/unit/resolver_spec.rb
@@ -6,6 +6,8 @@ describe 'Resolver' do
 
   let(:loader) { hashie load: :loaded_rules }
   let(:runner) { hashie run: :results }
+  let(:opts) {{}}
+  let(:rules) {[]}
 
   before do
     stub(Corals::Loader).new { loader }
@@ -13,13 +15,40 @@ describe 'Resolver' do
     stub(subject).applicable { :applicable_rules }
   end
 
-  after { subject.resolve }
+  after { subject.resolve opts, rules }
 
   context 'defaults' do
 
     it('loads') { mock(loader).load :applicable_rules }
 
     it('runs') { mock(runner).run :loaded_rules, {} }
+
+  end
+
+  context 'with opts' do
+
+    before do
+      opts[:name] = :name
+      stub(runner).compile { :compiled_rules }
+    end
+
+    it('compiles') { mock(runner).compile :loaded_rules }
+
+    it('parses') { mock(Corals::Parser).parse opts, :compiled_rules }
+
+  end
+
+  context 'anonymous rules' do
+
+    before { rules << anonymous_rules }
+
+    let(:anonymous_rules) {{x:42}}
+
+    it 'runs' do
+      mock(runner).run anything, opts do |actual_rules, _|
+        expect(actual_rules.rules.first.x).to eq 42
+      end
+    end
 
   end
 
