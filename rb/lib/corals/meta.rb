1abc1028ed89d77753bcd67a465ebd0a07eb78e2
mike <mikemps@gmail.com>
2018-02-28 22:34:30 -0500

    anonymous rules module

--- rb/lib/corals/meta.rb
+++ rb/lib/corals/meta.rb
@@ -1,3 +1,5 @@
+require 'securerandom'
+
 module Corals
 
   class Meta
@@ -7,7 +9,7 @@ module Corals
     end
 
     def rules
-      return @rules if anonymous_rules?
+      return anonymous_rules if anonymous_rules?
       return meta_rules if meta_rules?
       opts = @rules.nil? ? @opts :
         @opts.merge(rules: @rules)
@@ -19,7 +21,11 @@ module Corals
     def meta_rules; [:rules] end
 
     def anonymous_rules?; @rules and @rules[0].is_a?(Hash) end
-
+    def anonymous_rules
+      name = "rules_#{SecureRandom.hex}".to_sym
+      define "#{name}" => { rules: @rules }
+      [name]
+    end
   end
 
 end
