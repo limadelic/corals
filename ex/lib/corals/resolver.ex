47ac690f199cd027a9a99fadbf2f6566111b1f4b
maykels <maykel_suarez@ultimatesoftware.com>
2020-11-08 18:47:17 -0500

    cleanup

--- ex/lib/corals/resolver.ex
+++ ex/lib/corals/resolver.ex
@@ -3,24 +3,12 @@ defmodule Corals.Resolver do
   import Enum, only: [map: 2, reduce: 2, into: 2]
   import Map, only: [merge: 2]
 
-  def resolve rules do
-    if is_single? rules do
-      single rules
-    else
-      many rules
-    end
-  end
+  def resolve rules do if is_single? rules do single rules else many rules end end
 
   defp is_single? rules do Keyword.keyword? rules end
 
-  defp single rules do
-    rules |> into(%{})
-  end
+  defp single rules do rules |> into(%{}) end
 
-  defp many rules do
-    rules
-    |> map(&(single &1))
-    |> reduce(&(merge &2, &1))
-  end
+  defp many rules do rules |> map(&(single &1)) |> reduce(&(merge &2, &1)) end
 
 end
