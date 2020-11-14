9341c089a827fa057f66eda5e77b5706187dd86a
maykels <maykel_suarez@ultimatesoftware.com>
2020-11-14 11:22:04 -0500

    reduce_while many wip

--- ex/lib/corals/resolver.ex
+++ ex/lib/corals/resolver.ex
@@ -2,35 +2,39 @@ defmodule Corals.Resolver do
 
   import Corals.Expander
   import Corals.Utils
-  import Enum, only: [all?: 2, reduce: 3, reduce_while: 3]
+  import Enum, only: [all?: 2, reduce_while: 3]
 
   def resolve rules, opts \\ %{} do
     resolve rules, opts, opts
   end
 
   def resolve rules, context, opts do
+    process rules, context, opts
+  end
+
+  defp process rules, context, opts do
     cond do
-      is_single? rules -> single rules, context, opts
       are_many? rules -> many rules, context, opts
+      is_single? rules -> single rules, context, opts
       opts != %{} -> opts
       true -> rules
     end
   end
 
-  defp are_rules? rules do
-    is_list(rules) && (is_single?(rules) || are_many?(rules))
+  defp are_rules? rules do is_list(rules) && (is_single?(rules) || are_many?(rules)) end
+  defp are_many? rules do all? rules, &are_rules?/1 end
+  defp is_single? rules do Keyword.keyword? rules end
+
+  defp many rules, context, opts do
+    reduce_while rules, context, &(one_of_many(&1, &2, opts))
   end
 
-  defp is_single? rules do Keyword.keyword? rules end
+  defp one_of_many rules, context, opts do
+    {:cont, merge(context, process(rules, context, opts))}
+  end
 
   defp single rules, context, opts do
     reduce_while rules, context, &(expand &1, &2, opts)
-  end
-
-  defp are_many? rules do all? rules, &(are_rules? &1) end
-
-  defp many rules, context, opts do
-    reduce rules, context, &(merge &2, resolve(&1, &2, opts))
   end
 
 end
