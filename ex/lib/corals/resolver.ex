0d67775eb28852ce2d01267ab22f8ef99a121e6d
maykels <maykel_suarez@ultimatesoftware.com>
2020-11-14 18:54:18 -0500

    split many & single

--- ex/lib/corals/resolver.ex
+++ ex/lib/corals/resolver.ex
@@ -1,18 +1,13 @@
 defmodule Corals.Resolver do
 
-  import Corals.Expander
-  import Corals.Utils
-  import Enum, only: [all?: 2, reduce_while: 3]
+  import Corals.Many
+  import Corals.Single
 
   def resolve rules, opts \\ %{} do
     resolve rules, opts, opts
   end
 
   def resolve rules, context, opts do
-    process rules, context, opts
-  end
-
-  defp process rules, context, opts do
     cond do
       are_many? rules -> many rules, context, opts
       is_single? rules -> single rules, context, opts
@@ -21,20 +16,6 @@ defmodule Corals.Resolver do
     end
   end
 
-  defp are_rules? rules do is_list(rules) && (is_single?(rules) || are_many?(rules)) end
-  defp are_many? rules do all? rules, &are_rules?/1 end
-  defp is_single? rules do Keyword.keyword? rules end
-
-  defp many rules, context, opts do
-    reduce_while rules, context, &(one_of_many(&1, &2, opts))
-  end
-
-  defp one_of_many rules, context, opts do
-    {:cont, merge(context, process(rules, context, opts))}
-  end
-
-  defp single rules, context, opts do
-    reduce_while rules, context, &(expand &1, &2, opts)
-  end
+  def are_rules? rules do is_list(rules) && (is_single?(rules) || are_many?(rules)) end
 
 end
