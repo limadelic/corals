4d529d03d49071b98d661d1ca5995e901b775b52
maykels <maykel_suarez@ultimatesoftware.com>
2020-11-22 11:33:01 -0500

    trying to pick diff dominoes per player

--- ex/test/examples/dominoes/corals/players_rules.exs
+++ ex/test/examples/dominoes/corals/players_rules.exs
@@ -1,8 +1,7 @@
 defmodule Dominoes.Players do
 
   import Corals
-  import Enum, only: [take: 2, map: 2]
-  import Map, only: [put: 3]
+  import Enum, only: [take: 2]
 
   define :players, %{
     rules: [
@@ -17,9 +16,11 @@ defmodule Dominoes.Players do
       ],
       [
         when: is?(%{on: :pick}),
-        players: fn _, %{players: players, table: %{dominoes: dominoes}} ->
-          map(players, &(put &1, :dominoes, take(dominoes, 10)))
-        end
+        players: [
+          dominoes: fn _, %{table: %{dominoes: dominoes}} ->
+            take dominoes, 10
+          end
+        ]
       ]
     ]
   }
