a2b09d7c155b848f5e6c6e1c43ad76b38b8e8605
maykels <maykel_suarez@ultimatesoftware.com>
2020-11-30 11:23:33 -0500

    finding the best choices

--- ex/test/examples/mastermind/corals/mm_guess_rules.exs
+++ ex/test/examples/mastermind/corals/mm_guess_rules.exs
@@ -1,7 +1,7 @@
 defmodule MM.Guess do
 
   import Corals
-  import Enum, only: [random: 1]
+  import Enum, only: [random: 1, reduce: 3, uniq: 1, count: 2, at: 2]
 
   @colors [:red, :yellow, :green, :blue, :purple, :pink]
 
@@ -12,7 +12,20 @@ defmodule MM.Guess do
         choices: (
           for x1 <- @colors, x2 <- @colors, x3 <- @colors, x4 <- @colors,
             do: [x1, x2, x3, x4]
-        )
+        ),
+        choices: fn %{choices: choices} -> reduce choices, {[],[],[],[]},
+          fn choice, {best, better, good, bad} ->
+            count = length uniq choice
+            cond do
+              count == 1 -> {best, better, good, [choice | bad]}
+              count == 4 -> {best, better, [choice | good], bad}
+              count == 3 -> {best, [choice | better], good, bad}
+              count(choice, &(&1 == at(choice, 0))) == 2 ->
+                {[choice | best], better, good, bad}
+              true -> {best, better, good, [choice | bad]}
+            end
+          end
+        end
       ],
 #      [
 #        when: not?(%{choices: _}),
--- ex/test/examples/mastermind/test/mm_guess_test.exs
+++ ex/test/examples/mastermind/test/mm_guess_test.exs
@@ -2,15 +2,23 @@ defmodule MMGuessTest do
   use ExUnit.Case, async: true
 
   import Corals
+  import Corals.Helpers
 
-  @solution [:red, :green, :blue, :yellow]
+  describe "choices" do
 
-  setup do
-    %{solution: @solution} |> resolve(:mm_guess)
-  end
+    setup do resolve %{}, :mm_guess end
 
-  test "there r 1296 choices", %{choices: choices}do
-    assert length(choices) == 1296
+    test "there r 1296", %{choices: choices} do
+      assert choices |> Tuple.to_list |> Enum.map(&Enum.count/1) |> Enum.sum == 1296
+    end
+
+    test "they best choices have 2x2 colors", %{choices: {best, _, _, _}} do
+      assert Enum.all? best, fn choice ->
+        2 == length(Enum.uniq choice) &&
+        2 == Enum.count(choice, &(&1 == Enum.at choice, 0))
+      end
+    end
+
   end
 
   @tag :wip
