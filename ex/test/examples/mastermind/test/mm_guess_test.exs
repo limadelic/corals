8c35a20bbd640e0ee74b725bc3eacf6f103c62e9
maykels <maykel_suarez@ultimatesoftware.com>
2020-11-30 12:24:03 -0500

    finding rest of choices

--- ex/test/examples/mastermind/test/mm_guess_test.exs
+++ ex/test/examples/mastermind/test/mm_guess_test.exs
@@ -2,7 +2,6 @@ defmodule MMGuessTest do
   use ExUnit.Case, async: true
 
   import Corals
-  import Corals.Helpers
 
   describe "choices" do
 
@@ -12,10 +11,30 @@ defmodule MMGuessTest do
       assert choices |> Tuple.to_list |> Enum.map(&Enum.count/1) |> Enum.sum == 1296
     end
 
-    test "they best choices have 2x2 colors", %{choices: {best, _, _, _}} do
+    test "the best choices have 2x2 colors", %{choices: {best, _, _, _}} do
       assert Enum.all? best, fn choice ->
         2 == length(Enum.uniq choice) &&
         2 == Enum.count(choice, &(&1 == Enum.at choice, 0))
+      end
+    end
+
+    test "the better choices have 2X1X1 colors", %{choices: {_, better, _, _}} do
+      assert Enum.all? better, fn choice ->
+        3 == length Enum.uniq choice
+      end
+    end
+
+    test "the good choices have 1X1X1X1 colors", %{choices: {_, _, good, _}} do
+      assert Enum.all? good, fn choice ->
+        4 == length Enum.uniq choice
+      end
+    end
+
+    test "the bad choices are either 4 or 3 of the same color", %{choices: {_, _, _, bad}} do
+      assert Enum.all? bad, fn choice ->
+        1 == length(Enum.uniq choice) ||
+        3 == Enum.count(choice, &(&1 == Enum.at choice, 0)) ||
+        3 == Enum.count(choice, &(&1 == Enum.at choice, 1))
       end
     end
 
