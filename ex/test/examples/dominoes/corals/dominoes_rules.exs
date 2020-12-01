d3bf4bf67798dc749144d46418e16df055182d62
maykels <maykel_suarez@ultimatesoftware.com>
2020-12-01 12:24:41 -0500

    snake dominoes

--- ex/test/examples/dominoes/corals/dominoes_rules.exs
+++ ex/test/examples/dominoes/corals/dominoes_rules.exs
@@ -11,9 +11,17 @@ defmodule Dominoes do
   defp info %{on: {result, winner}, table: %{dominoes: dominoes}, players: players}= game do
     p "\n\n:: Dominoes ::\n"
     i result, winner
-    i :table, dominoes |> Enum.chunk_every(7)
+    i :table, snake dominoes
     i :players, Enum.map(players, &({&1.name, &1.count, &1.dominoes}))
     game
+  end
+
+  defp snake dominoes do
+    snake = dominoes |> Enum.chunk_every(7)
+    [
+      hd(snake) | tl(snake)
+      |> Enum.map_every(2, fn x -> x |> Enum.reverse |> Enum.map(&Enum.reverse/1) end)
+    ]
   end
 
   define __MODULE__, %{
