dca10102be0f17f87fc4e00341145a71772cc9f4
maykels <maykel_suarez@ultimatesoftware.com>
2020-12-01 12:01:42 -0500

    back to dominoes info

--- ex/test/examples/dominoes/corals/dominoes_rules.exs
+++ ex/test/examples/dominoes/corals/dominoes_rules.exs
@@ -4,20 +4,16 @@ defmodule Dominoes do
   import Corals.Helpers
 
   def play do play %{} end
-  def play %{on: {:winner, _}} = done do done end
-  def play %{on: {:tie, _}} = done do done end
-  def play game do game |> resolve(__MODULE__) |> info |> play end
+  def play %{on: {:winner, _}} = done do info done end
+  def play %{on: {:tie, _}} = done do info done end
+  def play game do game |> resolve(__MODULE__)  |> play end
 
-#  def info %{on: {result, winner}, table: %{dominoes: dominoes}, players: players}= game do
-#    p "\n\n:: Dominoes ::\n"
-#    i result, winner
-#    i :table, dominoes
-#    i :players, Enum.map(players, &({&1.name, &1.count, &1.dominoes}))
-#    game
-#  end
-  def info game do
+  defp info %{on: {result, winner}, table: %{dominoes: dominoes}, players: players}= game do
     p "\n\n:: Dominoes ::\n"
-    i game
+    i result, winner
+    i :table, dominoes |> Enum.chunk_every(7)
+    i :players, Enum.map(players, &({&1.name, &1.count, &1.dominoes}))
+    game
   end
 
   define __MODULE__, %{
--- ex/test/examples/dominoes/test/dominoes_test.exs
+++ ex/test/examples/dominoes/test/dominoes_test.exs
@@ -1,19 +1,9 @@
 defmodule DominoesTest do
   use ExUnit.Case
 
-  import Corals.Helpers
-
-  def count_dominoes dominoes, players do
-    Enum.count(dominoes) + Enum.sum(players |> Enum.map(&(Enum.count &1.dominoes)))
-  end
-
   test "play a whole game" do
-    %{on: {result, _}, table: %{dominoes: dominoes}, players: players} = Dominoes.play()
-
-
+    %{on: {result, _}} = Dominoes.play()
     assert result == :winner || result == :tie
-    i count_dominoes dominoes, players
-#    assert 40 = count_dominoes dominoes, players
   end
 
 end
