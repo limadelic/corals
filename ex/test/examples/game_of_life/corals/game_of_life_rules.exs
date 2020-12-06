e2cdd2cb172c77e8375986f25fe07c4c00dd181b
maykels <maykel_suarez@ultimatesoftware.com>
2020-12-06 09:17:13 -0500

    rename cells to game

--- ex/test/examples/game_of_life/corals/game_of_life_rules.exs
+++ ex/test/examples/game_of_life/corals/game_of_life_rules.exs
@@ -9,12 +9,12 @@ defmodule GoL do
     require: [GoL.Out]
   }
 
-  def evolve %{} = cells do resolve cells, __MODULE__ end
+  def evolve %{} = game do resolve game, __MODULE__ end
   def evolve cells do evolve(%{cells: cells}).cells end
   def evolve cells, 0 do cells end
-  def evolve %{} = cells, times do cells |> evolve |> info |> evolve(times - 1) end
+  def evolve %{} = game, times do game |> evolve |> evolve(times - 1) end
   def evolve cells, times do evolve %{cells: cells}, times end
 
-  def info %{cells: cells} do clear(); map cells, &(p &1); :timer.sleep 200; cells end
+  def info %{cells: cells} = game do clear(); map cells, &(p &1); :timer.sleep 200; game end
 
 end
