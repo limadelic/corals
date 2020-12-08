bd21cd877cb6446ff43607b0e40eec1a8ac0eda2
mike <mikemps@gmail.com>
2020-12-08 14:30:07 -0500

    small cleanup

--- ex/test/examples/game_of_life/test/examples_test.exs
+++ ex/test/examples/game_of_life/test/examples_test.exs
@@ -1,22 +1,24 @@
 defmodule GoL.Examples do
   use ExUnit.Case
 
+  @size [80, 25]
+
   @tag :gol
   @tag :glider
   test "glider" do
-    GoL.evolve %{cells: :glider, times: 50, size: [80, 25]}
+    GoL.evolve %{cells: :glider, times: 50, size: @size}
   end
 
   @tag :gol
   @tag :diamond
   test "diamond" do
-    GoL.evolve %{cells: :diamond, times: 25, size: [80, 25]}
+    GoL.evolve %{cells: :diamond, times: 25, size: @size}
   end
 
   @tag :gosper
   @tag :gol
   test "gosper_gun" do
-    GoL.evolve %{cells: :gosper_gun, times: 200, size: [80, 25]}
+    GoL.evolve %{cells: :gosper_gun, times: 200, size: @size}
   end
 
 end
