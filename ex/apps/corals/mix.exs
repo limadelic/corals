a3a24b039997ba480660ceeac5e6cde47f53ffc7
maykels <maykel_suarez@ultimatesoftware.com>
2020-12-10 10:46:32 -0500

    rename ex to corals

--- ex/apps/corals/mix.exs
+++ ex/apps/corals/mix.exs
@@ -3,7 +3,7 @@ defmodule Corals.MixProject do
 
   def project do
     [
-      app: :ex,
+      app: :corals,
       version: "0.1.0",
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
