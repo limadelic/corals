13533ca07c68b23fb6d2bfea0cfb49c722e1056e
maykels <maykel_suarez@ultimatesoftware.com>
2020-12-15 13:23:27 -0500

    loading corals

--- ex/apps/corals/lib/corals/loader.ex
+++ ex/apps/corals/lib/corals/loader.ex
@@ -1,12 +1,19 @@
 defmodule Corals.Loader do
 
+  import Path, only: [wildcard: 1]
+  import Enum, only: [uniq: 1, map: 2]
+
+  @corals_files "**/corals/**/*rules.exs"
+
   def start app do
     Task.Supervisor.start_child Corals.TaskSupervisor, &run/0
     app
   end
 
   def run do
-    IO.inspect File.cwd
+    corals_files |> map(&Code.require_file/1)
   end
+
+  defp corals_files do @corals_files |> wildcard |> uniq end
 
 end
