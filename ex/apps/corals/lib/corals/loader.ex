25be238b0b3dc145c2774cf3d619111efddd3bb8
maykels <maykel_suarez@ultimatesoftware.com>
2020-12-15 13:28:43 -0500

    resolving rules

--- ex/apps/corals/lib/corals/loader.ex
+++ ex/apps/corals/lib/corals/loader.ex
@@ -5,14 +5,9 @@ defmodule Corals.Loader do
 
   @corals_files "**/corals/**/*rules.exs"
 
-  def start app do
-    Task.Supervisor.start_child Corals.TaskSupervisor, &run/0
-    app
-  end
+  def start app do Task.Supervisor.start_child Corals.TaskSupervisor, &run/0; app end
 
-  def run do
-    corals_files |> map(&Code.require_file/1)
-  end
+  def run do corals_files() |> map(&Code.require_file/1) end
 
   defp corals_files do @corals_files |> wildcard |> uniq end
 
--- ex/apps/reef/lib/reef/controllers/rules_controller.ex
+++ ex/apps/reef/lib/reef/controllers/rules_controller.ex
@@ -1,8 +1,10 @@
 defmodule Reef.RulesController do
   use Reef, :controller
 
+  import String, only: [to_existing_atom: 1]
+
   def resolve conn, %{"rules" => rules} do
-    json conn, Corals.resolve(%{}, rules)
+    json conn, Corals.resolve(%{}, to_existing_atom(rules))
   end
 
 end
