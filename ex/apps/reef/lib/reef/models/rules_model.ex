0c5bdbe42fceb8716ba26b0d2ec458276dc6c7dd
msuarz <mikemps@gmail.com>
2020-12-17 11:18:00 -0500

    one liners

--- ex/apps/reef/lib/reef/models/rules_model.ex
+++ ex/apps/reef/lib/reef/models/rules_model.ex
@@ -3,18 +3,11 @@ defmodule Reef.RulesModel do
   import Reef.Helpers
   import Enum, only: [map: 2, filter: 2]
 
-  def index do
-    %{
-      rules: rules
-    }
-  end
+  def index do %{ rules: rules } end
 
-  defp rules do
-    Process.list
-    |> map(&Process.info/1)
-    |> Enum.filter(&(match? {Corals.Server, _, _}, &1[:dictionary][:"$initial_call"]))
-    |> map(&info/1)
-  end
+  defp rules do Process.list |> map(&Process.info/1) |> filter(&is_corals?/1) |> map(&info/1) end
+
+  defp is_corals? rules do match? {Corals.Server, _, _}, rules[:dictionary][:"$initial_call"] end
 
   defp info rules do
     %{
