0b5c804013c280766a96b21b7d15ef6f186b72dd
msuarz <mikemps@gmail.com>
2020-12-17 11:13:04 -0500

    showing just the name

--- ex/apps/reef/lib/reef/models/rules_model.ex
+++ ex/apps/reef/lib/reef/models/rules_model.ex
@@ -1,11 +1,24 @@
 defmodule Reef.RulesModel do
 
   import Reef.Helpers
+  import Enum, only: [map: 2, filter: 2]
 
   def index do
     %{
-      rules: [
-      ]
+      rules: rules
+    }
+  end
+
+  defp rules do
+    Process.list
+    |> map(&Process.info/1)
+    |> Enum.filter(&(match? {Corals.Server, _, _}, &1[:dictionary][:"$initial_call"]))
+    |> map(&info/1)
+  end
+
+  defp info rules do
+    %{
+      name: rules[:registered_name]
     }
   end
 
